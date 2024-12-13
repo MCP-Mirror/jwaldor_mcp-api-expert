1296a70538de5b9b6b0282856461f59f2a781ac1
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-12-13 13:52:40 -0500

    delete build

--- build/commands.js
+++
@@ -1,7 +0,0 @@
-// import { Command } from "commander";
-export {};
-// export function getSetupCommand() {
-//   return new Command("claude-setup").action((args) => {
-//     setupClaudeDesktop();
-//   });
-// }
